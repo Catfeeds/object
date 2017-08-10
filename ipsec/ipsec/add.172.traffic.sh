d413cea5b04bc161da2cf1392804dab20a24b33d
zhangchuang <chuang.zhang@ihaveu.net>
2017-08-10 14:19:11 +0800

    ipsec

---
+++ ipsec/ipsec/add.172.traffic.sh
@@ -0,0 +1,26 @@
+
+_reqid=1
+_local="172.20.2.0/24"
+_local_ip="172.20.2.102"
+_remote="10.0.0.0/16"
+_remote_ip="42.62.5.229"
+
+ip xfrm policy add \
+   src ${_remote} dst ${_local} \
+        dir fwd priority 2915 ptype main \
+        tmpl src ${_remote_ip} dst ${_local_ip} \
+                proto esp reqid ${_reqid} mode tunnel						
+
+ip xfrm policy add \
+   src ${_remote} dst ${_local} \
+        dir in priority 2915 ptype main \
+        tmpl src ${_remote_ip} dst ${_local_ip} \
+                proto esp reqid ${_reqid} mode tunnel						
+
+ip xfrm policy add \
+src ${_local} dst ${_remote} \
+        dir out priority 2915 ptype main \
+        tmpl src ${_local_ip} dst ${_remote_ip} \
+                proto esp reqid ${_reqid} mode tunnel
+                
+                
